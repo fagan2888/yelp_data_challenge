446f19ec4aa5e7a991f2cf143382d939df84d9bc
Maria Athena Beatrice Engesaeth <maria.enges@gmail.com>
2016-06-10 12:30:07 +0100

    update loading data

--- python/parse_business_data.py
+++ python/parse_business_data.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun 10 08:46:24 2016
-
 Read in Yelp data in pandas df
 
 @author: MariaAthena
@@ -11,12 +9,16 @@ import pandas as pd
 import json
 
 
-with open('yelp_academic_dataset_business.json', 'rb') as f:
+# read the entire file into a python array
+with open('../data/yelp_academic_dataset_business.json', 'rb') as f:
 	bus_data = f.readlines()
 
 
 bus_data = f.readlines()
+# remove the trailing "\n" from each line
 bus_data = map(lambda x: x.rstrip(), data)
+
+# put individual business JSON objects into list
 data_json = "[" + ','.join(bus_data) + "]"
 
 bus_df = pd.read_json(data_json)
