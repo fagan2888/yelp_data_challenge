76a72be222652880c5012643783d828b4ef5cfff
Maria Athena Beatrice Engesaeth <maria.enges@gmail.com>
2016-06-11 15:13:25 +0100

    write tip data parser

--- python/parse_tip_data.py
+++ python/parse_tip_data.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Read in Yelp data in pandas df
+Parse Yelp tip data
 
 @author: MariaAthena
 """
 
 import pandas as pd
+import numpy as np
 import json
-import collections
+import re
+import nltk
 
-from nltk.stem.porter import PorterStemmer # http://tartarus.org/martin/PorterStemmer/
-from nltk.stem import WordNetLemmatizer # http://textanalysisonline.com/nltk-wordnet-lemmatizer
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
 
 
 # Read data file into a python array
@@ -26,60 +29,85 @@ data_json = "[" + ','.join(bus_data) + "]"
 bus_df = pd.read_json(data_json)
 
 
-# Helper functions for preparing the text data
+## Helper functions to normalise and vectorise text
 
-# Convert all to lowercase and remove punctuation
-def clean_corpus(word_list):
+# x = nltk.collocations(text)
+
+# Convert all words to lowercase, remove punctuation, tokenise and stem
+# and remove stopwords, threshold = 10%
+def norm_corpus(document_list):
+    norm_doc_list = []
     
-    clean_words = []
-    word_list = [token.lower() for token in tokens]
+    # lowercase
+    document_list = [word.lower() for word in document_list]
+
+    
+    # remove symbols in text
+    symbols = ",.?!"
+    for sym in symbols:
+        document_list = [word.replace(sym,'') for word in document_list]
+    
+    
+    # loop through each string i.e. review in the column
+    for doc in document_list:
+        doc = nltk.word_tokenize(doc)
+        
+        # remove stopwords
+        doc = [word for word in doc if word not in stopwords.words('english')]
+        
+        # stem words
+        stemmer = SnowballStemmer("english")
+        doc = [stemmer.stem(word) for word in doc]
+        
+        # make tokenised text one string
+        norm_doc = " ".join(doc)
+        norm_doc_list.append(norm_doc)
+    
+    return norm_doc_list
 
 
 
-# Create list of words occurring too frequently, threshold: 10%
-def stop_words(word_list):
+# Vectorise normalised text to vector including only nouns and adjectives
+def review_vector(norm_doc_list):
+    review_list = []
 
-    stop_list = []
-    counts = collections.Counter(word_list)
-    frequency = counts[word] / float(len(word_list))
-
-    for word in word_list:
-        single_word_list = word_list.unique()
-
-        if frequency > 0.1:
-            stop_list.append(word)
-
-    return (single_word_list, stop_list)
-
-
-# Lemmatise 
-
-
-
-# Helper function to extract nouns from a Python string object
-def extract_nouns(txt):
-    nouns = []
-
-    # create list of words in a text, taking out punctuations, symbols etc.
-    words = nltk.word_tokenize(txt)
-    # categorise all words in text with tags
-    tags = nltk.pos_tag(words)
-
-    # select all words categorised as nouns
-    for word, pos in tags:
-        if (pos == 'NN' or pos == 'NNS' or pos == 'NNP' or pos == 'NNPS'):
-            nouns.append(word.lower())
-
-    return nouns
-
-
-# Helper function to convert text to vector
-WORD = re.compile(r'\w+')
-
-def text_to_vector(text):
-     words = WORD.findall(text)
-     return Counter(words)
+    # select all words categorised as nouns or adjectives
+    # loop through each string i.e. review in the df column
+    for doc in norm_doc_list:
+        review_keyword_list = []
+        doc = nltk.word_tokenize(doc)
+        # create tuple for each word in list: (word, tag)
+        token_category = nltk.pos_tag(doc)
+        
+        
+        for word, tag in token_category:    
+            
+            # nouns
+            if (tag == 'NN' or tag == 'NNS' or tag == 'NNP' or tag == 'NNPS'):
+                review_keyword_list.append(word)
+                
+            # adjectives
+            elif (tag == 'JJ' or tag == 'JJS' or tag == 'JJP' or tag == 'JJPS'):
+                review_keyword_list.append(word)
+            else:
+                pass     
+        
+        review_keywords = " ".join(review_keyword_list)
+        review_list.append(review_keywords)
+        
+        # vectorise string
+        WORD = re.compile(r'\w+')
+        review_vector = [collections.Counter(WORD.findall(word)) for word in review_list]
+    
+    
+    return review_vector
 
 
 
 
+# Normalise and vectorise tip column in datafram
+output_df = bus_df[['business_id', 'user_id', 'date']]  
+output_df['tip'] = norm_corpus(bus_df.text)
+output_df.tip = review_vector(output_df.tip)
+
+output_df.to_csv(open('parsed_tip_data.csv', 'wb'), index=False)
